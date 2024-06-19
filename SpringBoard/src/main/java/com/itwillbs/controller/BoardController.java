5c5a33ed8ff997d468ec795718fe571e3237dda5
peachk3 <doo062991@gmail.com>
2024-06-19 16:03:41 +0900

    #게시판 리스트 페이징 처리

--- SpringBoard/src/main/java/com/itwillbs/controller/BoardController.java
+++ SpringBoard/src/main/java/com/itwillbs/controller/BoardController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.itwillbs.domain.BoardVO;
+import com.itwillbs.domain.Criteria;
 import com.itwillbs.service.BoardService;
 
 @Controller
@@ -77,6 +78,24 @@ public class BoardController {
 		return"/board/list";
 	}
 	
+	// 게시판 글목록 조회 - GET
+	@RequestMapping(value ="/listPage",method = RequestMethod.GET )
+	public String listPageGET(Criteria cri, Model model) throws Exception {
+		logger.debug(" listALLGET() 실행 ");
+		// 페이징 처리 정보 객체
+		//		Criteria cri = new Criteria();
+		//		cri.setPage(1);
+		//		cri.setPageSize(10);
+		
+		// 서비스 -> DB의 정보를 가져오기 (페이징 처리)
+		List<BoardVO> boardList = bService.listPage(cri);
+		logger.debug(" size : " + boardList.size());
+		 
+		// 연결된 뷰페이지로 정보 전달 -> model 생성
+		model.addAttribute("boardList", boardList);
+		
+		return"/board/list";
+	}
 	
 	//http://localhost:8088/board/read?bno=1
 	// 게시판 본문 보기 - readGET
